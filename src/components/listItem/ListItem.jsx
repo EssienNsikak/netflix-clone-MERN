bdb3699761c9d35a1536c599e1663a5ac93bade2
EssienNsikak <nsikak.essienceo@gmail.com>
2021-07-19 19:16:28 +0100

    fetch movie feature implemented

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
 import './listItem.scss';
 import {
   PlayArrow,
@@ -5,12 +7,27 @@ import {
   ThumbUpAltOutlined,
   ThumbDownOutlined,
 } from '@material-ui/icons';
-import { useState } from 'react';
 
-export default function ListItem({ index }) {
+export default function ListItem({ index, item }) {
   const [isHovered, setIsHovered] = useState(false);
-  const trailer =
-    'https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761';
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    const getMovie = async () => {
+      try {
+        const res = await Axios.get('http://localhost:5004/api/movies/find/' + item, {
+          headers: {
+            token: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZjQ0NDI0YTUxZTRhNWExMmJlYTNmZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYyNjcxNTQ1MCwiZXhwIjoxNjI3MTQ3NDUwfQ.4uvl4tomVSh1RT97mK407wQI59FmxuXt2_H-dIY67xw'
+          }
+        });
+        setMovie(res.data)
+      } catch (err) {
+        console.log(err)
+      }
+    };
+    getMovie()
+  },[item])
+  
   return (
     <div
       className='listItem'
@@ -19,12 +36,12 @@ export default function ListItem({ index }) {
       onMouseLeave={() => setIsHovered(false)}
     >
       <img
-        src='https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee'
+        src={movie.img}
         alt=''
       />
       {isHovered && (
         <>
-          <video src={trailer} autoPlay={true} loop />
+          <video src={movie.trailer} autoPlay={true} loop />
           <div className='itemInfo'>
             <div className='icons'>
               <PlayArrow className='icon' />
@@ -33,14 +50,14 @@ export default function ListItem({ index }) {
               <ThumbDownOutlined className='icon' />
             </div>
             <div className='itemInfoTop'>
-              <span>1 hour 14 mins</span>
-              <span className='limit'>+16</span>
-              <span>1999</span>
+              <span>{movie.duration}</span>
+              <span className='limit'>+{movie.limit}</span>
+              <span>{movie.year}</span>
             </div>
             <div className='desc'>
-            The story follows Walt Kowalski, a recently widowed Korean War veteran alienated from his family and angry at the world...
+            {movie.desc}
             </div>
-            <div className='genre'>Action</div>
+            <div className='genre'>{movie.genre}</div>
           </div>
         </>
       )}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,17 +1,42 @@
+import { useState, useEffect } from 'react';
 import Featured from '../../components/featured/Featured';
 import List from '../../components/list/List';
 import Navbar from '../../components/navbar/Navbar';
+import Axios from 'axios';
 import './home.scss';
 
 const Home = ({ type }) => {
+
+  const [lists, setLists] = useState([]);
+  const [genre, setGenre] = useState(null);
+  
+  useEffect(() => {
+    const getRandomLists = async () => {
+      try {
+        const res = await Axios.get(
+          `http://localhost:5004/api/lists${type ? '?type=' + type : ''} ${genre ? '&genre=' + genre : ''}`, {
+            headers: {
+              token: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZjQ0NDI0YTUxZTRhNWExMmJlYTNmZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYyNjcxNTQ1MCwiZXhwIjoxNjI3MTQ3NDUwfQ.4uvl4tomVSh1RT97mK407wQI59FmxuXt2_H-dIY67xw'
+            }
+          }
+        );
+        setLists(res.data)
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getRandomLists()
+  }, [type, genre]);
+
   return (
     <div className='home'>
       <Navbar />
       <Featured type={type} />
-      <List />
-      <List />
-      <List />
-      <List />
+      {
+        lists.map((list) => (
+          <List list={list} />
+        ))
+      }
     </div>
   )
 };
